38e162d7a8c07107977b895829257f3cc83070cf
Diego Lopes Sakata <dihsakata@gmail.com>
2025-08-29 21:52:03 -0300

    learn: resolve atividade pw04
    - cria metodos personalizados no dominio Produto

---
+++ pw04/web04_exemplo/loja/src/main/java/com/fatec/loja/ProdutoRepository.java
@@ -0,0 +1,20 @@
+package com.fatec.loja;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository 
+extends JpaRepository<Produto, Integer>{
+    @Query(value = "select * from produto where destaque > 0 order by destaque"
+    , nativeQuery = true)
+    public List<Produto> ListarVitrine();
+
+    @Query(value = "SELECT * FROM produto WHERE keywords LIKE ?1"
+    , nativeQuery = true)
+    public List<Produto> fazerBusca(String termo);
+
+}
